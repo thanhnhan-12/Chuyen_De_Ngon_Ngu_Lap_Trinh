1fe7933958c56b4f0f04f1e08df24b10ee15ec2b
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-08-28 20:45:12 +0700

    Thanh Nhan

---
+++ BT1_CAI_DAT_MOI_TRUONG/NguyenThanhNhan_1911505310242.rb
@@ -0,0 +1,25 @@
+# Bài 1: Viết lệnh hiển thị "Xin chào" + tên SV + MSV
+print("Xin chào " + "Nguyễn Thành Nhân " + "1911505310242 \n")
+
+# Bài 2: Nhập 1 số từ bàn phím, in ra số đó
+print ("\nNhap mot so bat ky: ")
+number_242 = gets
+puts "=> #{number_242}"
+
+# Bài 3: So sánh 2 giá trị nhập vào từ bàn phím, in ra giá trị lớn hơn
+puts "\nNhap so thu nhat: "
+No1_242 = gets
+# puts "=> #{No1_242}"
+puts "Nhap so thu hai: "
+No2_242 = gets
+
+if (No1_242 > No2_242)
+    puts "=> #{No1_242}"
+else
+    puts "=> #{No2_242}"
+
+
+end
+
+
+
