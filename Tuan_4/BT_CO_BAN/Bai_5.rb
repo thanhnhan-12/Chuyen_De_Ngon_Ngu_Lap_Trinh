6f0789dd74586e59cd9ca791fca8b6584da7189f
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-09-16 22:21:37 +0700

    Bai 5

---
+++ Tuan_4/BT_CO_BAN/Bai_5.rb
@@ -0,0 +1,10 @@
+# Viết chương trình để in một số nguyên, một số thực, một ký tự đã được định nghĩa trước.
+I_242 = 5
+print "So nguyen: ", I_242
+
+F_242 = 1.1
+print "\nSo thuc: ", F_242
+
+S_242 = 'n'
+print "\nKy tu: ", S_242
+
