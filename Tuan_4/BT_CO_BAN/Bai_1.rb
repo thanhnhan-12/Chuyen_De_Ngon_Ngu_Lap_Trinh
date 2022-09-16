4e43e391475d90f4446f246ea6bbe08b8a4bff64
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-09-16 21:34:48 +0700

    Bai 2

--- Tuan_4/BT_CO_BAN/Bai_1.rb
+++ Tuan_4/BT_CO_BAN/Bai_1.rb
@@ -1,10 +1,18 @@
-array = []
+array_242 = []
+Sum_242 = 0
 print "Nhap so phan tu: "
-number = gets
-for i in 1..number.to_i do 
-    print "array" + "[#{i}]: "
-    x = gets
-    array.push x
+numbers_242 = gets
+for i_242 in 1..numbers_242.to_i do 
+    print "array" + "[#{i_242}]: "
+    x_242 = gets
+    array_242.push x_242
+    # Sum_242 += array_242
 end
-print "Gia tri lon nhat cua mang: " + array.max
-print "Gia tri nho nhat cua mang: " + array.min
+
+# for i_242 in 1..numbers_242.to_i do 
+#     Sum_242 = Sum_242 + x_242
+# end
+
+print "Gia tri lon nhat cua mang: " + array_242.max
+print "Gia tri nho nhat cua mang: " + array_242.min
+# print "Gia tri trung binh cua mang: #{Sum_242 / array_242.length} " 
--- Tuan_4/BT_CO_BAN/Bai_2.rb
+++ Tuan_4/BT_CO_BAN/Bai_2.rb
@@ -1,2 +1,14 @@
-if(a == b && a == c && b == c) then
-   
+# Nhập 3 cạnh tam giác, kiểm tra điều kiện có phải 3 cạnh tam giác hay ko? nếu đúng --> tính chu vi, diên tích --> in ra
+print "Nhap canh AB: "
+AB_242 = gets.to_i
+print "Nhap canh BC: "
+BC_242 = gets.to_i
+print "Nhap canh AC: "
+AC_242 = gets.to_i
+  if AB_242 + BC_242 > AC_242 && AB_242 + AC_242 > BC_242 && BC_242 + AC_242 > AB_242 
+    print "Day la tam giac " 
+    print "\nChu vi: ", Perimeter_242 = (AB_242 + BC_242 + AC_242) / 2.0
+    print "\nDien tich: ", Area_242 = Math.sqrt(Perimeter_242 * (Perimeter_242 - AB_242) * (Perimeter_242 - AC_242) * (Perimeter_242 - BC_242)).to_f
+  else
+    puts "Khong phai tam giac! "
+end
