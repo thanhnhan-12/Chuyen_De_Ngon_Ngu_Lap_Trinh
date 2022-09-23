c637444eb0fe787603d38d5e30fd582e483e0686
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-09-23 14:19:48 +0700

    Update information Student Management

--- Tuan_5/BT3_OOP/StudentManagement.rb
+++ Tuan_5/BT3_OOP/StudentManagement.rb
@@ -22,7 +22,7 @@ class Human
   attr_accessor :name, :hair, :weight, :height, :age, :phone, :email, :nation
 
   def showInformation
-    puts "Fullname: #{name} \nHair: #{hair} \nWeight: #{weight} \nHeight: #{height} \nAge: #{age} \nPhone: #{phone} \nEmail: #{email} \nNational: #{nation}"
+    puts "\nFullname: #{name} \nHair: #{hair} \nWeight: #{weight} \nHeight: #{height} \nAge: #{age} \nPhone: #{phone} \nEmail: #{email} \nNational: #{nation}"
   end
   
 end
@@ -47,11 +47,17 @@ class Student < Human
 
   def showInformation 
     super
-    puts "Grade 1: #{grade_1} \nGrade 2: #{grade_2} \nGrade 3: #{grade_3} \nAverage: #{average}   "
+    puts "Grade 1: #{grade_1} \nGrade 2: #{grade_2} \nGrade 3: #{grade_3} \nAverage: #{average}"
   end
 
 end
 
 arrayStudent = Array.new
-student_1 = Student.new("Nhan", "Black", 58, 58, 21, "0935489523", "nguyenthanhnhan12701@gmail.com", "Vietnam", 9, 10, 7, "Avarage: #{avarage} ")
-student_2 = Student.new("Sy", "Black", 58, 58, 21, "0905215823", "sybuivan@gmail.com", "Vietnam", 9, 8, 6, "Avarage: #{avarage} ")
+student_1 = Student.new("Nhan", "Black", 58, 58, 21, "0935489523", "nguyenthanhnhan12701@gmail.com", "Vietnam", 9, 10, 7).showInformation
+student_2 = Student.new("Sy", "Black", 70, 68, 21, "0905215823", "sybuivan@gmail.com", "Vietnam", 9, 7, 6).showInformation
+student_3 = Student.new("Huy", "Black", 60, 88, 21, "0905215823", "sybuivan@gmail.com", "Vietnam", 7, 8, 10).showInformation
+
+arrayStudent.push(student_1, student_2, student_3)
+arrayStudent.each do |student|
+  puts "#{student}"
+end 
