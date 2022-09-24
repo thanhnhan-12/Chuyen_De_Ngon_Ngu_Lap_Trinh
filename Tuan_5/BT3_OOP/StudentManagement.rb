75f604e9f7b57a2f4ebc44bd86a324f9155bee8a
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-09-24 10:48:53 +0700

    Change  variables naming  Grades

--- Tuan_5/BT3_OOP/StudentManagement.rb
+++ Tuan_5/BT3_OOP/StudentManagement.rb
@@ -9,13 +9,13 @@ class Human
   @nation
 
   def initialize(name,hair,weight,height,age,phone,email,nation)
-    @name = name
-    @hair = hair
+    @name   = name
+    @hair   = hair
     @weight = weight 
     @height = height
-    @age = age
-    @phone = phone
-    @email = email
+    @age    = age
+    @phone  = phone
+    @email  = email
     @nation = nation
   end
 
@@ -28,37 +28,36 @@ class Human
 end
 
 class Student < Human 
-  @grade_1
-  @grade_2
-  @grade_3
+  @mathGrades
+  @physicalGrades
+  @chemistryGrades
 
-  def initialize(name,hair,weight,height,age,phone,email,nation, grade_1, grade_2, grade_3)
+  def initialize(name,hair,weight,height,age,phone,email,nation, mathGrades, physicalGrades, chemistryGrades)
     super(name,hair,weight,height,age,phone,email,nation)
-    @grade_1 = grade_1
-    @grade_2 = grade_2
-    @grade_3 = grade_3
+    @mathGrades      = mathGrades
+    @physicalGrades  = physicalGrades
+    @chemistryGrades = chemistryGrades
   end
 
-  attr_accessor :grade_1, :grade_2, :grade_3
+  attr_accessor :mathGrades, :physicalGrades, :chemistryGrades
 
   def average
-    (@grade_1 + @grade_2 + @grade_3 ) / 3.0
+    (@mathGrades + @physicalGrades + @chemistryGrades ) / 3.0
   end
 
   def showInformation 
     super
-    puts "Grade 1: #{grade_1} \nGrade 2: #{grade_2} \nGrade 3: #{grade_3} \nAverage: #{average}"
+    puts "Grade 1: #{mathGrades} \nGrade 2: #{physicalGrades} \nGrade 3: #{chemistryGrades} \nAverage: #{average}"
   end
 
 end
 
-arrayStudent = Array.new
+arrayStudent = []
 student_1 = Student.new("Nhan", "Black", 58, 58, 21, "0935489523", "nguyenthanhnhan12701@gmail.com", "Vietnam", 9, 10, 7).showInformation
 student_2 = Student.new("Sy", "Black", 70, 68, 21, "0905215823", "sybuivan@gmail.com", "Vietnam", 9, 7, 6).showInformation
 student_3 = Student.new("Huy", "Black", 60, 88, 21, "0905215823", "sybuivan@gmail.com", "Vietnam", 7, 8, 10).showInformation
 
-arrayStudent.push(student_1, student_2, student_3)
-arrayStudent.each do |student|
-  puts "#{student}"
-end 
+arrayStudent.push(student_1)
+arrayStudent.push(student_2)
+arrayStudent.push(student_3)
 
