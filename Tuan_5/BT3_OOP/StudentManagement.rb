e532a2f9b4c839a8acb69c188217d1a506739a29
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-09-24 13:44:32 +0700

    Implement feature/StudentManagement

--- Tuan_5/BT3_OOP/StudentManagement.rb
+++ Tuan_5/BT3_OOP/StudentManagement.rb
@@ -1,12 +1,6 @@
 class Human 
-  @name
-  @hair
-  @weight
-  @height
-  @age
-  @phone
-  @email
-  @nation
+
+  attr_accessor :name, :hair, :weight, :height, :age, :phone, :email, :nation
 
   def initialize(name,hair,weight,height,age,phone,email,nation)
     @name   = name
@@ -19,8 +13,6 @@ class Human
     @nation = nation
   end
 
-  attr_accessor :name, :hair, :weight, :height, :age, :phone, :email, :nation
-
   def showInformation
     puts "\nFullname: #{name} \nHair: #{hair} \nWeight: #{weight} \nHeight: #{height} \nAge: #{age} \nPhone: #{phone} \nEmail: #{email} \nNational: #{nation}"
   end
@@ -28,9 +20,8 @@ class Human
 end
 
 class Student < Human 
-  @mathGrades
-  @physicalGrades
-  @chemistryGrades
+
+  attr_accessor :mathGrades, :physicalGrades, :chemistryGrades
 
   def initialize(name,hair,weight,height,age,phone,email,nation, mathGrades, physicalGrades, chemistryGrades)
     super(name,hair,weight,height,age,phone,email,nation)
@@ -38,8 +29,6 @@ class Student < Human
     @physicalGrades  = physicalGrades
     @chemistryGrades = chemistryGrades
   end
-
-  attr_accessor :mathGrades, :physicalGrades, :chemistryGrades
 
   def average
     (@mathGrades + @physicalGrades + @chemistryGrades ) / 3.0
@@ -53,13 +42,26 @@ class Student < Human
 end
 
 arrayStudent = []
-student_1 = Student.new("Nhan", "Black", 58, 58, 21, "0935489523", "nguyenthanhnhan12701@gmail.com", "Vietnam", 9, 10, 7).showInformation
-student_2 = Student.new("Sy", "Black", 70, 68, 21, "0977258231", "sybuivan@gmail.com", "Vietnam", 9, 7, 6).showInformation
-student_3 = Student.new("Huy", "Black", 60, 88, 21, "0985215823", "sybuivan@gmail.com", "Vietnam", 7, 8, 10).showInformation
-student_4 = Student.new("Thien", "Black", 70, 98, 11, "0905444826", "vanthienpham@gmail.com", "Vietnam", 7, 10, 10).showInformation
+student_1 = Student.new("Nhan", "Black", 58, 58, 44, "0935489523", "nguyenthanhnhan12701@gmail.com", "Vietnam", 9, 10, 7)
+student_2 = Student.new("Sy", "Black", 70, 68, 50, "0977258231", "sybuivan@gmail.com", "Vietnam", 9, 7, 6)
+student_3 = Student.new("Huy", "Black", 60, 88, 32, "0985215823", "sybuivan@gmail.com", "Vietnam", 7, 8, 10)
+student_4 = Student.new("Thien", "Black", 70, 98, 11, "0905444826", "vanthienpham@gmail.com", "Vietnam", 7, 10, 10)
 
 arrayStudent.push(student_1)
 arrayStudent.push(student_2)
 arrayStudent.push(student_3)
 arrayStudent.push(student_4)
+
+puts "====== Unsorted student list :======= "
+for i in 0..(arrayStudent.length-1)
+    puts "=====Student #{i+1} ==== "
+    puts arrayStudent[i].showInformation
+ end
+
+puts "====== Sorted student list :======= "
+studentsort = arrayStudent.sort_by{|student| [student.average]}.reverse
+for i in 0..(studentsort.length-1)
+  puts "===== Student #{i+1} ===== "
+  puts studentsort[i].showInformation
+end
 
